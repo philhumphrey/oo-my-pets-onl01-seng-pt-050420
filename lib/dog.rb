7b8504cc66e52121548100f87118c33dcf394232
Phil Humphrey <phumphrey588@yahoo.com>
2020-06-04 04:50:54 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
+
 class Dog
-  # code goes here
+  @@all = []
+  attr_accessor :mood ,:owner
+  attr_reader :name
+  
+  @@instances = []
+  
+  def initialize(name, owner)
+  @name = name
+  @owner = owner
+  @mood = "nervous"
+  @@instances << self 
+  end
+  
+  def self.all 
+    @@instances
+end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,69 @@
+
 class Owner
-  # code goes here
+attr_accessor :pets
+attr_reader :name, :species
+# @@count = 0
+@@instances = []
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@instances << self
+  end
+  
+  def cats
+    Cat.all.select  {|x| x.owner == self}
+    end
+  
+  def dogs
+    Dog.all.select {|x| x.owner ==self}
+  end
+  
+  def buy_cat(x)
+    Cat.new(x, self)
+  end
+  
+  def buy_dog(y)
+    Dog.new(y, self)
+  end
+  
+  def walk_dogs
+    Dog.all.each {|x| x.mood = "happy"}
+  end
+  
+  def feed_cats
+    Cat.all.each {|x| x.mood = "happy"}
+  end
+  
+  def sell_pets
+    Cat.all.each do |x| 
+      x.mood = "nervous"
+      x.owner = nil
+    end
+    Dog.all.each do |x|
+      x.mood = "nervous"
+      x.owner = nil
+    end
+  end
+  
+  def list_pets
+    "I have #{dogs.count} dog(s), and #{cats.count} cat(s)."
+  end
+    
+  def say_species
+    "I am a #{@species}."
+  end
+  
+  def self.all
+    @@instances
 end
+
+def self.count
+ @@instances.count
+end
+
+def self.reset_all
+  @@instances.clear
+  end
+end
+  
